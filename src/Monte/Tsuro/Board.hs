4ee71654f198ecc0ac2bf9f3e4d5a8cdde3a4870
Micah Hahn <micah.s.hahn@gmail.com>
2018-11-19 08:31:15 -0500

    Initial Tsuro Commit

---
+++ src/Monte/Tsuro/Board.hs
@@ -0,0 +1,72 @@
+ {-# LANGUAGE OverloadedStrings #-}
+ 
+ module Monte.Tsuro.Board (
+     Player(..),
+     Game(..),
+     playTile
+ ) where
+
+import Data.Array
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
+import Data.Text (Text)
+import System.Random
+
+import Monte.Tsuro.Tile
+
+newtype BoardIx = BoardIx (Int, Int)
+    deriving (Show, Eq, Ord, Ix)
+
+newtype BoardPos = BoardPos { unBoardPos :: (BoardIx, Int) }
+    deriving (Show)
+
+data Player = Player
+    { position :: BoardPos
+    , tiles :: Vector Tile
+    } deriving (Show)
+
+data Game = Game
+    { board :: Array BoardIx (Maybe Tile)
+    , players :: Vector Player
+    , drawTiles :: Vector Tile
+    , seed :: StdGen
+    } deriving (Show)
+
+mapPath :: BoardIx -> Int -> Maybe BoardPos
+mapPath (BoardIx (x, y)) p
+    | p >= 0 && p <= 1 = mkPos (y == 0) (x, y - 1)
+    | p >= 2 && p <= 3 = mkPos (x == 7) (x + 1, y)
+    | p >= 4 && p <= 5 = mkPos (y == 7) (x, y + 1)
+    | p >= 6 && p <= 7 = mkPos (x == 0) (x - 1, y)
+    where mkPos b ix = if b then Nothing else Just $ BoardPos ((BoardIx ix), (mapping Vector.! p))
+          mapping = Vector.fromList [5, 4, 7, 6, 1, 0, 3, 2]
+    
+{- Returns a the new board position or nothing if the player is forced off the board -}
+followPaths :: Array BoardIx (Maybe Tile) -> BoardPos -> Maybe BoardPos
+followPaths b p = let (BoardPos (ix, pos)) = p
+                  in case b ! ix of
+                        Nothing -> Just p    
+                        Just (Tile paths _) -> case mapPath ix (paths Vector.! pos) of
+                            Nothing -> Nothing
+                            Just newPos -> followPaths b newPos
+
+{- playTile :: Game 
+         -> Int          {- Player number -}
+         -> Tile         {- Tile to be played -}
+         -> Either Text a -}
+playTile game playerIx tile = do
+    let player = players game Vector.! playerIx
+    if any (tileEq tile) (tiles player) then pure () else Left ("This tile is not available to be played" :: Text)
+    let newBoard = (board game) // [(fst . unBoardPos . position $ player, Just tile)]
+    let newPos = followPaths newBoard (position player)
+    Right newPos
+
+blankBoard = array (BoardIx (0, 0), BoardIx (7, 7)) [(BoardIx (x, y), Nothing) | x <- [0..7], y <- [0..7]]
+
+testGame = Game { board = blankBoard 
+                , players = Vector.fromList [ Player (BoardPos (BoardIx (0, 0), 0)) (Vector.fromList (take 3 allTiles))
+                                            , Player (BoardPos (BoardIx (7, 7), 4)) (Vector.fromList (take 3 . drop 3 $ allTiles)) 
+                                            ]
+                , drawTiles = Vector.fromList . drop 6 $ allTiles
+                , seed = mkStdGen 0
+                }
---
+++ src/Monte/Tsuro/Tile.hs
@@ -0,0 +1,108 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Monte.Tsuro.Tile (
+    Tile(..),
+    allTiles,
+    rotateTile,
+    tileEq
+) where
+
+import Data.List
+import Data.Tuple
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Data.Monoid ((<>))
+
+data Tile = Tile 
+    { paths :: Vector Int
+    , rotations :: Int 
+    } deriving (Show, Eq)
+
+tileEq :: Tile -> Tile -> Bool
+tileEq (Tile l _) (Tile r _) = isEquivalent l r
+
+{- rotates the tile clockwise -}
+rotate :: Vector Int -> Vector Int
+rotate t = Vector.imap (\i v -> ((t Vector.! (i `addMod8` 6))) `addMod8` 2) t
+    where addMod8 x y = (x + y) `mod` 8
+
+rotateTile :: Tile -> Tile
+rotateTile (Tile ps r) = Tile (rotate ps) r
+
+gen :: [Int] -> [[(Int, Int)]]
+gen [] = [[]]
+gen (x:xs) = concat $ [((x,y):) <$> (gen $ delete y xs) | y <- xs]
+
+makePaths :: [(Int, Int)] -> Vector Int
+makePaths xs = let x1 = Vector.update (Vector.replicate 8 0) (Vector.fromList xs)
+                   x2 = Vector.update x1 (swap <$> Vector.fromList xs)
+               in x2
+
+isEquivalent :: Vector Int -> Vector Int -> Bool
+isEquivalent l r = l == r 
+                || rotate l == r 
+                || (rotate . rotate) l == r 
+                || (rotate . rotate . rotate) l == r
+
+unorderedGroupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+unorderedGroupBy _ [] = []
+unorderedGroupBy eq (x:xs) = let (l, r) = partition (eq x) xs
+                             in (x : l) : unorderedGroupBy eq r
+
+allTiles :: [Tile]
+allTiles = let groups = unorderedGroupBy isEquivalent $ makePaths <$> gen [0..7]
+           in (\g -> Tile (head g) (length g)) <$> groups
+
+data SvgPrim' a = SvgLine { start :: a, end :: a }
+                | SvgBeizer3 { start :: a, control :: a, end :: a }
+                | SvgBeizer4 { start :: a, control1 :: a, control2 :: a, end :: a }
+                deriving (Functor)
+
+newtype SvgPoint = SvgPoint { unSvgPoing :: (Double, Double) }
+
+type SvgPrim = SvgPrim' SvgPoint
+
+primToSvgString :: SvgPrim -> Text
+primToSvgString x = case x of 
+    (SvgLine s e) -> "M " <> getPoint s <> " L " <> getPoint e
+    (SvgBeizer3 s c e) -> "M " <> getPoint s <> " Q " <> getPoint c <> ", " <> getPoint e
+    (SvgBeizer4 s c1 c2 e) -> "M " <> getPoint s <> " C " <> getPoint c1 <> ", " <> getPoint c2 <> ", " <> getPoint e
+    
+    where getPoint (SvgPoint (x, y)) = (Text.pack . show $ x) <> " " <> (Text.pack . show $ y)
+
+scale :: Double -> SvgPrim -> SvgPrim
+scale s p = (\(SvgPoint (x, y)) -> SvgPoint (x * s, y * s)) <$> p
+
+{- Only draw where starting index < ending index -}
+{- renderTile :: Tile -> [SvgPrim] -}
+renderTile (Tile paths _) = scale 100 . renderPath <$> liftedPaths
+    where liftedPaths :: Vector (Int, Int)
+          liftedPaths = Vector.filter (\(p1, p2) -> p1 < p2) $ Vector.imap (\i v -> (i, v)) paths
+
+          renderPath :: (Int, Int) -> SvgPrim
+          renderPath (p1, p2)
+              | p1 > 1 = fmap rotateRight $ renderPath ((p1 - 2) `mod` 2, (p2 - 2) `mod` 8)
+              | p1 == 1 = fmap flipHorizontal $ renderPath (flipH p1, flipH p2)
+              | otherwise = render0To p2
+
+          flipH :: Int -> Int
+          flipH i = [1, 0, 7, 6, 5, 4, 3, 2] !! i
+
+          render0To :: Int -> SvgPrim
+          render0To 1 = SvgBeizer4 (SvgPoint (1/3, 0)) (SvgPoint (1/3, 0.22)) (SvgPoint (2/3, 0.22)) (SvgPoint (2/3, 0))
+          render0To 2 = SvgBeizer4 (SvgPoint (1/3, 0)) (SvgPoint (1/3, 1/3)) (SvgPoint (2/3, 1/3)) (SvgPoint (1, 1/3))
+          render0To 3 = SvgBeizer3 (SvgPoint (1/3, 0)) (SvgPoint (1/3, 2/3)) (SvgPoint (1, 2/3))
+          render0To 4 = SvgBeizer4 (SvgPoint (1/3, 0)) (SvgPoint (1/3, 2/3)) (SvgPoint (2/3, 1/3)) (SvgPoint (2/3, 1))
+          render0To 5 = SvgLine (SvgPoint (1/3, 0)) (SvgPoint (1/3, 1))
+          render0To 6 = SvgBeizer4 (SvgPoint (1/3, 0)) (SvgPoint (1/3, 1/3)) (SvgPoint (1/3, 2/3)) (SvgPoint (0, 2/3))
+          render0To 7 = SvgBeizer3 (SvgPoint (1/3, 0)) (SvgPoint (1/3, 1/3)) (SvgPoint (0, 1/3))
+
+          rotateRight :: SvgPoint -> SvgPoint
+          rotateRight (SvgPoint (x, y)) = SvgPoint (1 - y, x)
+
+          flipHorizontal :: SvgPoint -> SvgPoint
+          flipHorizontal (SvgPoint (x, y)) = SvgPoint (x, 1 - y)
+
