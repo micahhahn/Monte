f2111b1ed73cacac8028567b2399f229a4d6c7db
Micah Hahn <micah.s.hahn@gmail.com>
2018-11-28 09:25:08 -0500

    Remove gap in game pieces

--- src/Monte/LucidExtensions.hs
+++ src/Monte/LucidExtensions.hs
@@ -8,6 +8,9 @@ import Data.Text
 path_ :: Applicative m => [Attribute] -> HtmlT m ()
 path_ = with (makeElementNoEnd "path")
 
+viewbox_ :: Text -> Attribute
+viewbox_ = makeAttribute "viewbox"
+
 d_ :: Text -> Attribute
 d_ = makeAttribute "d"
 
